6835d1b0e7a97da62004509952deaa7570167428
Josse <jossedavila@gmail.com>
2023-01-01 17:12:59 -0500

    Added third html page with an animated background

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ links = {"insta":["https://www.instagram.com/josse_and_the_larvitars/?hl=en", ".
 }
 keys = Object.keys(links);
 function populateUL() {
-    var ul = document.getElementById("list");
+    var ul = document.getElementById("list")
     for(let i = 0; i < keys.length; i++){
-        var li = document.createElement("li");
+        var li = document.createElement("li")
         li.innerHTML = '<img src="./images/pokeball.svg" width="20"/>'
             + '<img src="' + links[keys[i]][1] + '"width="20"/>'
             + '<a href  = "' +links[keys[i]][0] + '"> ' 
-            + links[keys[i]][2] + '</a>';
-        li.setAttribute("id", "element4"); 
-        ul.appendChild(li);
+            + links[keys[i]][2] + '</a>'
+        li.setAttribute("id", "element4") 
+        ul.appendChild(li)
     }
+    console.log('done')
 }
 
